a616f0ab6b20584c928c150620ce1b2a539a3cb4
Anthony <xetqL@users.noreply.github.com>
2020-09-07 15:43:58 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <type_traits>
+
+struct A {
+    int field;
+    int foo() {return 0;}
+};
+
+template <typename, typename = void> struct has_foo_function : std::false_type {};
+template <typename T> struct has_foo_function<T, std::void_t< decltype(T().foo()) >> : std::true_type {};
+
+template <typename, typename = void> struct has_field : std::false_type {};
+template <typename T> struct has_field<T, std::void_t< decltype(T::field) > >: std::true_type {};
+
+int main()
+{
+    std::cout << has_foo_function<A>::value << std::endl;
+    std::cout << has_field<A>::value << std::endl;
+
+    std::cout << has_foo_function<int>::value << std::endl;
+    std::cout << has_field<int>::value << std::endl;
+    
+    return 0;
+}
